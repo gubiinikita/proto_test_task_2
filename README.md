7015bea3ec809ccef49a07a1bfc34f7e63ae1741
clear <clear@MacBook-Pro-Nik.local>
2023-02-03 14:29:06 +0200

    simple solution implemented, deviation numbers prints in console

--- README.md
+++ README.md
@@ -8,15 +8,14 @@ Tests should check that maximum deviation of dice results
 is within 5% First test: for 1 dice Second test:
 for 2 dices (total roll points is a sum of 2 dices)
 ## How to run this test:
-You can run command with default params (number of rolls = 10000,
-count of dices = 1)
+You can run command with default params (number of rolls = 10000)
 ```
 npm run test
 ```
 Or you can run command with params e.g count
-of dices(DICE_COUNT) or num of rolls (ROLLS_NUM)
+of dices(DICE_COUNT)
 ```
-ROLLS_NUM=5000 DICE_COUNT=2 npm run test
+ROLLS_NUM=5000 npm run test
 ```
 ## Results analysis
 If maximum deviation of dice results
--- test/helper.js
+++ test/helper.js
@@ -1,34 +1,32 @@
-import superagent from "superagent";
 const expect = require('chai').expect;
 
+export const avg = (rollsNum, min, max) => rollsNum / (max - min + 1);
+export const requestUrl = (rollsNum, min, max) => `https://www.random.org/integers/?num=${rollsNum}&min=${min}&max=${max}&col=1&base=10&format=plain&rnd=new`;
+
 export const deviationChecker = (numbersArray, avg) => {
-    const numbersCount = {}
+    const numbersCount = {};
     numbersArray.forEach(number => {
-        if(numbersCount[number]){
+        if (numbersCount[number]) {
             numbersCount[number]++
         } else {
             numbersCount[number] = 1
         }
     });
     console.log(`Distribution ${JSON.stringify(numbersCount, null, 1)}`);
-    const result = Object.values(numbersCount).every(count => {
-        const deviation = count / avg
-        return deviation <= 1.05 && deviation >= 0.95
+    let isTestFailed = false;
+    const deviationNumbers = {};
+    Object.entries(numbersCount).forEach(([number, count]) => {
+        const deviation = count / avg;
+        const isDeviationWithinMargin = deviation <= 1.05 && deviation >= 0.95;
+        if (!isDeviationWithinMargin) {
+            isTestFailed = true;
+            deviationNumbers[number] = count;
+        }
     });
-    expect(result).to.be.true;
-    console.log(`Is maximum deviation of dice results is within 5%? ${result.toString()}`);
-}
-export const roll = async (diceCount, rollsCount) => {
-    const requestUrl = `https://www.random.org/integers/?num=${rollsCount}&min=1&max=6&col=1&base=10&format=plain&rnd=new`;
-    const promiseArray = [];
-    for (let i = 0; i < diceCount; i++) {
-        promiseArray.push(superagent.get(requestUrl));
+    if(isTestFailed == true){
+        console.log(`Maximum deviation of dice results isn't within 5%. Deviation numbers ${JSON.stringify(deviationNumbers, null, 1)}`)
+    } else {
+        console.log(`Maximum deviation of dice results is within 5%`);
     }
-    const responses = await Promise.all(promiseArray);
-    const resArray = responses.map(res => res.text.split(`\n`).filter(Boolean));
-    return resArray.reduce((accumulator, currentValue) => {
-        return accumulator.map((number, index) => {
-            return Number(number) + Number(currentValue[index]);
-        });
-    });
-};
+    expect(isTestFailed).to.be.false;
+}
