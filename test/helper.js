2530f78be4d64a598fdb3c8ca28e43c379828a42
clear <clear@MacBook-Pro-Nik.local>
2023-02-23 12:35:08 +0200

    fixed

--- test/helper.js
+++ test/helper.js
@@ -1,8 +1,12 @@
 const expect = require('chai').expect;
 import superagent from "superagent";
+import config from "../env/config";
+
+const {baseUrl} = config;
 
 export const deviationChecker = (numbersArray, avg) => {
     const numbersCount = {};
+
     numbersArray.forEach(number => {
         if (numbersCount[number]) {
             numbersCount[number]++
@@ -10,33 +14,41 @@ export const deviationChecker = (numbersArray, avg) => {
             numbersCount[number] = 1
         }
     });
+
     console.log(`Distribution ${JSON.stringify(numbersCount, null, 1)}`);
+
     let isTestFailed = false;
     const deviationNumbers = {};
+
     Object.entries(numbersCount).forEach(([number, count]) => {
         const deviation = count / avg;
-        const isDeviationWithinMargin = deviation <= 1.05 && deviation >= 0.95;
+        const isDeviationWithinMargin = deviation <= 1.025 && deviation >= 0.975;
         if (!isDeviationWithinMargin) {
             isTestFailed = true;
             deviationNumbers[number] = count;
         }
     });
-    if(isTestFailed == true){
+
+    if (isTestFailed == true) {
         console.log(`Maximum deviation of dice results isn't within 5%. Deviation numbers ${JSON.stringify(deviationNumbers, null, 1)}`)
     } else {
         console.log(`Maximum deviation of dice results is within 5%`);
     }
+
     expect(isTestFailed).to.be.false;
 }
 
 export const roll = async (diceCount, rollsCount) => {
-    const requestUrl = `https://www.random.org/integers/?num=${rollsCount}&min=1&max=6&col=1&base=10&format=plain&rnd=new`;
+    const requestUrl = `${baseUrl}/integers/?num=${rollsCount}&min=1&max=6&col=1&base=10&format=plain&rnd=new`;
     const promiseArray = [];
+
     for (let i = 0; i < diceCount; i++) {
         promiseArray.push(superagent.get(requestUrl));
     }
+
     const responses = await Promise.all(promiseArray);
     const resArray = responses.map(res => res.text.split(`\n`).filter(Boolean));
+
     return resArray.reduce((accumulator, currentValue) => {
         return accumulator.map((number, index) => {
             return Number(number) + Number(currentValue[index]);
--- test/test.js
+++ test/test.js
@@ -1,13 +1,20 @@
 import {deviationChecker, roll} from './helper';
+import config from "../env/config";
+
+const {baseDiceCount, baseRollsNum} = config;
+
 describe('Dices', () => {
-    const diceCount = process.env.DICE_COUNT || 1;
-    const rollsNum = process.env.ROLLS_NUM || 10000;
+    const diceCount = process.env.DICE_COUNT || baseDiceCount;
+    const rollsNum = process.env.ROLLS_NUM || baseRollsNum;
     const avg = rollsNum / 6;
 
     console.log('Count of dices', diceCount)
+
     it(`${diceCount} dices`, async () => {
         console.log(`Roll with parameters: number of rolls [${rollsNum}], count of dices [${diceCount}]`);
+
         const resArray = await roll(diceCount, rollsNum);
+
         deviationChecker(resArray, avg);
     });
 });
